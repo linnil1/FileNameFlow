e7a2c141da6d3adf1a8e81dc43e17f3b02a5d342
linnil1 <linnil1.886@gmail.com>
2022-06-10 19:58:51 +0800

    Add document and deploy code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Name-based Pipeline
+
+Check document in HackMD (Still writing)
+
+https://hackmd.io/ogyqCAUZQjaGuw1Mv5BGjw
+
+::: namepipe.error
+::: namepipe.path
+::: namepipe.task
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="namepipe",
+    version="0.0.1",
+    author="linnil1",
+    author_email="linnil1.886@gmail.com",
+    description="A name-based pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/linnil1/NamePipe",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "parse",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.10",
+)
